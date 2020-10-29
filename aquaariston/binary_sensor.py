16f4ce8707f9973fd94a493fecbd2071f46a85bc
Janos Haber <boci.boci@gmail.com>
2020-10-29 22:07:20 +0100

    Update binary_sensor.py
    Add some device type

--- aquaariston/binary_sensor.py
+++ aquaariston/binary_sensor.py
@@ -3,6 +3,8 @@
 from homeassistant.components.binary_sensor import (
     DEVICE_CLASS_CONNECTIVITY,
     DEVICE_CLASS_HEAT,
+    DEVICE_CLASS_POWER,
+    DEVICE_CLASS_HEAT,
     BinarySensorEntity,
 )
 from homeassistant.const import CONF_BINARY_SENSORS, CONF_NAME
@@ -41,8 +43,8 @@ BINARY_SENSORS = {
     PARAM_ONLINE: (BINARY_SENSOR_ONLINE, DEVICE_CLASS_CONNECTIVITY, None),
     PARAM_CHANGING_DATA: (BINARY_SENSOR_CHANGING_DATA, None, "mdi:cogs"),
     PARAM_UPDATE: (BINARY_SENSOR_UPDATE, None, "mdi:package-down"),
-    PARAM_ON: (BINARY_SENSOR_ON, None, "mdi:power"),
-    PARAM_HEATING: (BINARY_SENSOR_HEATING, None, "mdi:fire"),
+    PARAM_ON: (BINARY_SENSOR_ON, DEVICE_CLASS_POWER, "mdi:power"),
+    PARAM_HEATING: (BINARY_SENSOR_HEATING, DEVICE_CLASS_HEAT, "mdi:fire"),
     PARAM_CLEANSE: (BINARY_SENSOR_CLEANSE, None, "mdi:bacteria-outline"),
     PARAM_ECO: (BINARY_SENSOR_ECO, None, "mdi:leaf"),
 }
